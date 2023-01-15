88a70569012cfef88bcb41c530527215ecb5be10
Sunder91 <a_sunder_91@hotmail.com>
2023-01-14 21:46:02 -0600

    start of link page

--- BurnoutGaming/Controllers/Streamers.cs
+++ BurnoutGaming/Controllers/Streamers.cs
@@ -18,6 +18,12 @@ namespace BurnoutGaming.Controllers
         return View();
 
         }
+        public IActionResult Links()
+        {
+            
+            return View();
+            
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
